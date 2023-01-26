c92c48be05d287f21188b4daa71b4725a90bd576
Manuel Tovar <tovarmanuel569@gmail.com>
2023-01-26 17:22:28 -0500

    actualizacion de seccion detalles

--- app.js
+++ app.js
@@ -4,12 +4,18 @@ const btnAnterior = document.getElementById("anterior");
 const urlBase = "https://api.themoviedb.org/3/";
 const apiKey = "api_key=c9b993483c9002e040faf6c3550d97fd";
 const urlImg = "https://image.tmdb.org/t/p/original/";
+const detalles = document.getElementById("detalles");
+const video = document.getElementById("video");
 let contador = 1;
+let contenedorPelicula = document.getElementById("contenedor");
 
 btnSiguiente.addEventListener("click", ()=>{
     if(contador < 1000){
         contador++
+        detalles.innerHTML = ""
+        video.innerHTML = ""
         cargarpelicula();
+        
     }
 })
 
@@ -17,6 +23,8 @@ btnAnterior.addEventListener("click", ()=>{
 
     if(contador > 1){
         contador--
+        detalles.innerHTML = ""
+        video.innerHTML = ""
         cargarpelicula();
     }
 })
@@ -25,30 +33,33 @@ formulario.addEventListener("submit", buscarPelicula);
 
 function buscarPelicula(e){
     e.preventDefault();
+    
     const buscar = document.getElementById("buscar").value;
    consultarApi(buscar);
+   detalles.innerHTML = ""
+   video.innerHTML = ""
 }
 
 async function consultarApi(buscar){
-    const url = `${urlBase}search/movie?${apiKey}&query=${buscar}`
-
+    
     try {
-        const respuesta = await fetch(url);
-        const resultado = await respuesta.json();
-
+       const api = await fetch(`${urlBase}search/movie?${apiKey}&query=${buscar}`)
+        .then(respuesta => respuesta.json())
+        .then(resultado => resultado)
+        
         let peliculas = "";
-        resultado.results.forEach(async (pelicula)=>{
+        api.results.forEach(async (pelicula)=>{
             peliculas += `
             <ul>
                 <li class="peliculas">
-                    <img class="img-pelicula" src="${urlImg}${pelicula.poster_path}">
+                   <a href="#formulario" > <img class="img-pelicula" src="${urlImg}${pelicula.poster_path}" onclick="detallePelicula(${pelicula.id})"> </a>
                     <h3>${pelicula.title}</h3>
                     <p>${pelicula.vote_average}${" "}⭐</p>
                 </li>
             </ul>
             `
         })
-        document.getElementById("contenedor").innerHTML = peliculas
+        contenedorPelicula.innerHTML = peliculas
 
     } catch (error) {
         console.log(error)
@@ -57,33 +68,77 @@ async function consultarApi(buscar){
 
 const cargarpelicula = async() => {
     try {
-        const respuesta = await fetch(`${urlBase}movie/popular?${apiKey}&language=es-ES&page=${contador}`);
-        const datos = await respuesta.json();
+
+        const api = await fetch(`${urlBase}movie/popular?${apiKey}&language=es-ES&page=${contador}`)
+        .then(respuesta => respuesta.json())
+        .then(resultado => resultado)
+
         let peliculas = "";
         
-            datos.results.forEach( async(pelicula) => {
+            api.results.forEach( async(pelicula) => {
                 peliculas += `
                 
                 <ul>
-                    <li class="peliculas">
-                        <img class="img-pelicula" src="${urlImg}${pelicula.poster_path}">
+                    <li id="${pelicula.id}" class="peliculas">
+                    <a href="#formulario" > <img class="img-pelicula" src="${urlImg}${pelicula.poster_path}" onclick="detallePelicula(${pelicula.id})"> </a>
                         <h3>${pelicula.title}</h3>
                         <p>${pelicula.vote_average}${" "}⭐</p>
                     </li>
                 </ul>
             `
+           
         });
 
-        document.getElementById("contenedor").innerHTML = peliculas;
-
+        contenedorPelicula.innerHTML = peliculas;
+        
     } catch(error){
         console.log(error);
     }
 
 }
 
+async function detallePelicula(id){
+    try {
+        const api = await fetch(`${urlBase}movie/${id}?${apiKey}&language=es-ES&page=${contador}`)
+        .then(respuesta => respuesta.json())
+        .then(resultado => resultado)
+
+        detalles.innerHTML = `
+                
+                <img class="img-pelicula" src="${urlImg}${api.poster_path}">
+                <img class="fondo" src="${urlImg}${api.backdrop_path}">
+                <div id="info" >
+                <h2>${api.title}</h2>
+                <p>${api.vote_average}${" "}⭐</p>
+                <h3>Descripción :</h3>
+                <h4>${api.overview}</h4>
+                <h4> Lanzamiento : ${api.release_date}  </h4>
+                </div>
+                
+        `
+        console.log(api)
+        videos(api.id)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 cargarpelicula();
 
+async function videos(id){
+    try {
+        
+        const api = await fetch(`${urlBase}movie/${id}/videos?${apiKey}&language=es-ES&page=${contador}`)
+        .then(respuesta => respuesta.json())
+        .then(resultado => resultado)
 
+        video.innerHTML = `
+        <iframe width="660" height="415" src="https://www.youtube.com/embed/${api.results[0].key}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        `
+
+    } catch (error) {
+        video.innerHTML = ""
+    }
+}
 
 
